f43dc0fc0b7ac3520911e57b771f84f1c2c905a7
Vahag <135336961+Vahag5@users.noreply.github.com>
2023-07-06 18:03:13 +0400

    Update 17_06_2.py

--- 17_06_2.py
+++ 17_06_2.py
@@ -2,16 +2,21 @@ phone_book = {}
 def contacts():
     global phone_book
     while True:
-        q = input("Continiue added contact Yes/No ? ")
-        if q == 'Yes' or q == 'Y' or q == 'y':
-            pass
+        q = input("if You want added new contact press '1'\n"
+"if you want search contact press:  '2'\n"
+"if you want see all contacts press: '3'\n"
+"if you want exit the program press: '4'\n")
+        if q == '1':
+            n = input("name: ")
+            p = input("phone_number ")
+            print('Contact added successfully!')
+            phone_book['name'] = n
+            phone_book['phone_number'] = p
+        elif q == '2':
+        elif q == '3':   
         else: 
-            break   
-        n = input("name: ")
-        p = input("phone_number ")
-        print('Contact added successfully!')
-        phone_book['name'] = n
-        phone_book['phone_number'] = p
+             break  
+        
     return phone_book
 contacts()
 print(phone_book)
